83be22964844f74059815cb9fbe9a41bf738b7fe
Alana MM <alanamm.prof@gmail.com>
2019-04-04 10:44:59 -0300

    Aula 04/04

---
+++ base de dados exemplos/bancoJoins.sql
@@ -0,0 +1,26 @@
+﻿CREATE TABLE basket_a (
+    id INT PRIMARY KEY,
+    fruit VARCHAR (100) NOT NULL
+);
+ 
+CREATE TABLE basket_b (
+    id INT PRIMARY KEY,
+    fruit VARCHAR (100) NOT NULL
+);
+ 
+INSERT INTO basket_a (id, fruit)
+VALUES
+    (1, 'Apple'),
+    (2, 'Orange'),
+    (3, 'Banana'),
+    (4, 'Cucumber');
+ 
+INSERT INTO basket_b (id, fruit)
+VALUES
+    (1, 'Orange'),
+    (2, 'Apple'),
+    (3, 'Watermelon'),
+    (4, 'Pear'),
+    (5, 'Ananas');
+
+
